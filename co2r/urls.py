2245725fe7cdcc12b85c29bf5cfadd14cc2a143c
pierre.drescher@gmail.com <pierre.drescher@gmail.com>
2012-09-10 09:50:14 -0400

    updated urls

--- co2r/urls.py
+++ co2r/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^$', TemplateView.as_view(template_name='base.html'), name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/products$', 'co2r.products.views.products', name='products'),
     url(r'^api/products/(?P<internal_name>[a-z0-9-_]+$)$', 'co2r.products.views.product', name='product'),
