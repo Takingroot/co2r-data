01fdad64b231ac42db654a505970ebfca84b1e8f
pierre.drescher@gmail.com <pierre.drescher@gmail.com>
2012-10-05 11:43:00 -0400

    other actions tied to artifact instead of footprint

--- co2r/artifacts/models.py
+++ co2r/artifacts/models.py
@@ -148,7 +148,7 @@ class Footprint(models.Model, TranslatedModelMixin):
 
 
 class OtherAction(models.Model, TranslatedModelMixin):
-    footprint = models.ForeignKey(Footprint)
+    artifact = models.ForeignKey(Artifact)
     name = models.CharField(max_length=100)
     name_fr = models.CharField(max_length=100)
     description = models.TextField()
