430b153fc04a2a308485031587528afa85d91f7c
pierre.drescher@gmail.com <pierre.drescher@gmail.com>
2012-10-11 11:54:37 -0400

    updates to admin

--- co2r/organizations/admin.py
+++ co2r/organizations/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from co2r.organizations.models import Organization, ContactInfo
 
-admin.site.register(Organization, admin.ModelAdmin)
-admin.site.register(ContactInfo, admin.ModelAdmin)
+
+class ContactInfoInline(admin.TabularInline):
+    model = ContactInfo
+    extra = 0
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    inlines = [ContactInfoInline]
+
+
+admin.site.register(Organization, OrganizationAdmin)
