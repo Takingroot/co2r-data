086659af1c42dbd67c5e2ab90a378be243db0c19
Pierre <pierre.drescher@gmail.com>
2012-10-25 09:39:44 -0400

    made x-origin header apply to all responses, not just API requests references #29

--- co2r/middleware.py
+++ co2r/middleware.py
@@ -1,12 +1,9 @@
 from dynamicresponse.middleware.dynamicformat import DynamicFormatMiddleware
 from dynamicresponse.response import DynamicResponse
 
-
-class Co2rApiMiddleware(DynamicFormatMiddleware):
-    def process_response(self, request, response):
-        if isinstance(response, DynamicResponse):
-            response = response.render_response(request, response)
-            response['Access-Control-Allow-Origin'] = '*'
-            response['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
-            response['Access-Control-Allow-Headers'] = 'X-Requested-With'
+class Co2rMiddleware(object):
+    def process_response(self, request, response):    
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+        response['Access-Control-Allow-Headers'] = 'X-Requested-With'
         return response
--- co2r/settings/common.py
+++ co2r/settings/common.py
@@ -120,7 +120,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'dynamicresponse.middleware.api.APIMiddleware',
-    'co2r.middleware.Co2rApiMiddleware',
+    'co2r.middleware.Co2rMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
