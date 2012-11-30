f16ae7c77670b46f430230509694955844adaaf5
Pierre <pierre.drescher@gmail.com>
2012-11-30 16:16:46 -0500

    added organizations views

--- co2r/organizations/views.py
+++ co2r/organizations/views.py
@@ -1,1 +1,18 @@
+from dynamicresponse.response import SerializeOrRender
+from co2r.organizations.models import Organization
+
+
+def organizations(request):
+    """
+    Returns a list of all artifacts
+    """
+    organizations = Organization.objects.all()
+
+    language_code = request.GET.get('language', None)
+    if language_code != None:
+        for organization in organizations:
+            organization.set_language(language_code)
+
+    return SerializeOrRender('organizations/list.html', {'organizations': organizations})
+
 # Create your views here.
--- co2r/urls.py
+++ co2r/urls.py
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
         url(r'^api/app$', 'co2r.main.views.app_content', name='app_content'),
         url(r'^api/artifacts$', 'co2r.artifacts.views.artifacts', name='artifacts'),
         url(r'^api/artifact/(?P<slug>[a-z0-9-_]+)$', 'co2r.artifacts.views.artifact', name='artifact'),
+        url(r'^api/organizations$', 'co2r.organizations.views.organizations', name='organizations'),
         url(r'^api/footprints$', 'co2r.artifacts.views.footprints', name='footprints'),
         url(r'^api/footprint/(?P<slug>[a-z0-9-_]+)/(?P<year>[0-9]+)$', 'co2r.artifacts.views.footprint', name='footprint')
     )
